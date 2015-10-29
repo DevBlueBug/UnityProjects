03d15b4f0453b82f76d65dde8a7bb71601ebd0db
DevBlueBug <devbluebug@gmail.com>
2015-10-29 17:49:39 +0900

    I got a job. Finally. 00 01 I am going to make money now?

---
+++ Work/Job_Chicken/Assets/Scripts/Game/Character/Character.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Game.Character{
+	
+	public class Character : MonoBehaviour {
+		CharacterDels.Health_Changed E_Health =  delegate{};
+		CharacterDels.Hunger_Changed E_Hunger =  delegate{};
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+
+}
---
+++ Work/Job_Chicken/Assets/Scripts/Game/Character/CharacterDels.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Game.Character
+{
+	public class CharacterDels
+	{
+		public delegate void Health_Changed(double change);
+		public delegate void Hunger_Changed(double change);
+
+
+	}
+}
+
